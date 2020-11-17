a237671d9d3d102ab82b95ddd70805323fa5cbae
codefuncode <codefuncode@protonmail.com>
2020-11-16 21:19:38 -0400

    Cambios en la navegacion por las ofertas

--- web/js/selectofertas.js
+++ web/js/selectofertas.js
@@ -30,23 +30,101 @@ function pruebaseleccion(argument) {
                 // --- 5 de noviembre se cambio esta linea para delegar los datos en la pagina 
                 var nombreOfertaDisplay = document.getElementById('nombreOfertaDisplay');
 
-                var imagenserver = document.getElementById('imagenserver');
+                // var imagenserver = document.getElementById('imagenserver');
                 var videoserver = document.getElementById('videoserver');
 
                 // video.style.display = "none";
 
                 nombreOfertaDisplay.innerHTML = data[data.length - 1]['nombreOferta'];
-                imagenserver.src = "php/" + data[data.length - 1]['imagen'];
+                // imagenserver.src = "php/" + data[data.length - 1]['imagen'];
                 console.log(data[data.length - 1]['video']);
+                videoserver.src = "php/" + data[data.length - 1]['video'];
 
-                var srt = data[data.length - 1]['video'];
-                videoserver.src = srt.slice(3, 13);
+                console.log(data.length - 1);
+
+                // var srt = data[data.length - 1]['video'];
+
+                // videoserver.src = srt.slice(3, 13);
+
+            }).always(function(data) {
+
+                if (data) {
+
+                    var
+                        prev = document.getElementById('prev'),
+                        next = document.getElementById('next'),
+                        indiceimg = document.getElementById('indiceimg'),
+                        imajenofertaactual = document.getElementById('imajenofertaactual'),
+                        list = [],
+                        posicion = 0,
+                        captionText = document.getElementById('CaptionText');
+
+                    for (var i = 0; i < data.length - 1; i++) {
+
+                        list[i] = document.createElement("spam");
+                        list[i].classList.add("dot");
+                    }
+                    for (var i = 0; i < list.length; i++) {
+
+                        indiceimg.appendChild(list[i]);
+                    }
+
+                    for (var i = 0; i < list.length; i++) {
+
+                        console.log(list[i]);
+                    }
+
+                }
+                imajenofertaactual.src = "php/" + data[posicion]['imagen'];
+                captionText.innerHTML = data[posicion]['nombreOferta'];
+                prev.addEventListener("click", cambiaOferta);
+                next.addEventListener("click", cambiaOferta);
+
+                function cambiaOferta(argument) {
+                    console.log(this);
+                    console.log(data.length);
+                    console.log((posicion > data.lengt - 1));
+
+                    if (this == next) {
+
+                        posicion += 1;
+
+                        if (posicion > data.length - 1) {
+                            posicion = 0;
+                        }
+
+                        console.log('=====next=======');
+                        console.log(posicion);
+                        console.log('======next======');
+
+                    } else if (this == prev) {
+                        posicion -= 1;
+
+                        if (posicion < 0) {
+                            posicion = data.length - 1;
+                        }
+                        console.log('=======prev=====');
+                        console.log(posicion);
+                        console.log('=======prev=====');
+                    }
+
+                    console.log(posicion);
+                    imajenofertaactual.src = "php/" + data[posicion]['imagen'];
+                    captionText.innerHTML = data[posicion]['nombreOferta'];
+                }
 
             })
             .fail(function(data) {
                 console.log('fail');
                 console.log(data);
             });
+
+        var puruebimg = document.querySelectorAll('.mySlides img');
+
+        for (var i = 0; i < puruebimg.length; i++) {
+            console.log(puruebimg[i]);
+        }
+
     };
     iniciodata();
 
